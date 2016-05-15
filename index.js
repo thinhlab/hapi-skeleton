79c72ce9a703d48ccb9debb9030cdddbba0e15f8
Thinh Nguyen <thinhit@yahoo.com>
2016-05-15 16:02:42 +0700

    Fix require manifest path

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Hapi      = require("hapi");
 const fs        = require('fs');
 const Glue      = require('glue');
 
-Glue.compose(require('./configs/manifest'), { relativeTo: process.cwd() }, function(error, server) {
+Glue.compose(require('./manifest'), { relativeTo: process.cwd() }, function(error, server) {
     if(error){
         throw new Error(error);
     }
