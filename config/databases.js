02cbcdd60e97ce78c590404353440625c1bee511
Thinh Nguyen <thinhit@yahoo.com>
2016-05-18 00:14:21 +0700

    update

---
+++ config/databases.js
@@ -0,0 +1,14 @@
+module.exports = {
+    'orders': {
+        "host": "127.0.0.1",
+        "user": "root",
+        "password": "",
+        "database": "bxm_orders"
+    },
+    'metadata': {
+        "host": "127.0.0.1",
+        "user": "root",
+        "password": "",
+        "database": "bxm_metadata"
+    }
+}
